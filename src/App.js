57b79227ab7c5ad05f6e6036ef3c2a3bc8ce50e6
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-09-05 12:29:35 -0300

    testando JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 
 function App() {
+  const name = <h1>Erick</h1>;
+  const age = <h2> idade</h2>;
+  const email = <h2> email@domain.com</h2>
+  const user = (
+    <div>
+      {name}
+      {age}
+      {email}
+    </div>
+  );
   return (
     <div className="App">
+      {user}
+      {user}
+      {user}
       
     </div>
   );

c03a259e297f8d0885cadbdcb8df8322dc7ca86b
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-09-06 12:47:43 -0300

    Props

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <User/>
-      <User/>
-      <User/>
+      <User name="Erick" age={32} email="erick@mail.com"/>
+      <User name="Jake" age={25} email="jake@mail.com"/>
+      <User name="Jessica" age={37} email="jessica@mail.com"/>
     </div>
   );
 }
 
-const User = () => {
+const User = (props) => {
   return (
     <div>
-      <h1> Erick</h1>
-      <h1> idade</h1>
-      <h1> email@domain.com</h1>
+      <h1> {props.name}</h1>
+      <h1> {props.age}</h1>
+      <h1> {props.email}</h1>
     </div>
   );
 }
